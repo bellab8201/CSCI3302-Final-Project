3ed03e15ea40e579de3da7ddc1be9cecfa52b443
Olivia Milas <49049722+omilas47@users.noreply.github.com>
2023-05-07 14:51:23 -0600

    update manipulation
    added code block, top shelf control

--- controllers/grocery_shopper/grocery_shopper.py
+++ controllers/grocery_shopper/grocery_shopper.py
@@ -127,75 +127,24 @@ keyboard.enable(timestep)
 # ------------------------------------------------------------------
 # Helper Functions
 
-mode = 'manual'
-#gripper_status="open"
-"""
-arm_1_joint.setPosition(0.7)
-arm_2_joint.setPosition(0.5)
-arm_3_joint.setPosition(-3.46)
-arm_4_joint.setPosition(0.1)
-arm_5_joint.setPosition(0.1)
-arm_6_joint.setPosition(0.1)
-arm_7_joint.setPosition(0.1)
-"""
+mode = 'manipulation'
 
-gripper_status = 0
-current_position = 0
-"""
-#target_position = [-4.34, -0.00468, 0.0238]
-target_position = [0.0049, 0.0153, -0.1208]
-print("The angles of each joints are : ", my_chain.inverse_kinematics(target_position))
-real_frame = my_chain.forward_kinematics(my_chain.inverse_kinematics(target_position))
-print("Computed position vector : %s, original position vector : %s" % (real_frame[:3, 3], target_position))
-"""
-
-current_joint_positions = np.zeros(12)
-joint_positions = my_chain1.forward_kinematics(current_joint_positions)
-print("===Printing Forward Kinematics===")
-print(joint_positions)
-
-#target_position_object = [4.4, -3.53, 1.07]
-#print("The angles of each joints are : ", my_chain1.inverse_kinematics(target_position_object))
-"""
-target = [4.4, -3.53, 1.07]
-target_orientation = [-6.32976e-16, -1, -6.62413e-16]
-
-frame_target = np.eye(4)
-frame_target[:3, 3] = target
-
-ik = my_chain1.inverse_kinematics_frame(frame_target)
-"""
 # ------------------------------------------------------------------
 # Main Loop
 while robot.step(timestep) != -1:
 
 # ------------------------------------------------------------------
     #ARM MOVEMENT
-    #i = my_chain1.inverse_kinematics([2,2,2])
-    #print(i)
-    
-    #arm_1_joint.setPosition(0.7)   
-    
-    arm_1_joint_sensor = robot.getDevice('arm_1_joint_sensor').getValue()
-    arm_2_joint_sensor = robot.getDevice('arm_2_joint_sensor').getValue()
-    arm_3_joint_sensor = robot.getDevice('arm_3_joint_sensor').getValue()
-    arm_4_joint_sensor = robot.getDevice('arm_4_joint_sensor').getValue()
-    arm_5_joint_sensor = robot.getDevice('arm_5_joint_sensor').getValue()
-    arm_6_joint_sensor = robot.getDevice('arm_6_joint_sensor').getValue()
-    arm_7_joint_sensor = robot.getDevice('arm_7_joint_sensor').getValue()
-    #print(arm_1_joint_sensor,arm_2_joint_sensor,arm_3_joint_sensor,arm_4_joint_sensor,arm_5_joint_sensor,arm_6_joint_sensor, arm_7_joint_sensor)
-    
-
     robot_parts["wheel_left_joint"].setVelocity(vL)
     robot_parts["wheel_right_joint"].setVelocity(vR)
     #arm controller
-    if mode == 'manual':
+    if mode == 'manipulation':
         key = keyboard.getKey()
         while(keyboard.getKey() != -1): pass
         # Move the shoulder lift joint right/left
         #if key == ord('1'):
         if key==Keyboard.CONTROL+ord('B'):
-        #position to get cubes in basket - DONT USE THIS ONE IF TESTING ON MIDDLE SHELF
+        #position to get cubes in basket
             arm_1_joint.setPosition(0.07)
             arm_2_joint.setPosition(0.2)
             arm_3_joint.setPosition(-1.5)
@@ -211,8 +160,7 @@ while robot.step(timestep) != -1:
         # --- BLOCKS MIDDLE SHELF --- #    
         elif key==Keyboard.CONTROL+ord('M'):
         #BLOCKS ON MIDDLE SHELF
-        #waypoint 1: 1.35,-3.02, 0.0881 aisle 2 first block on mid shelf
-        #waypoint 2: 1.61, -3.02, 0.0884
+            torso_lift_joint.setPosition(0)
             arm_1_joint.setPosition(0.07)
             arm_2_joint.setPosition(0.04)
             arm_3_joint.setPosition(-1.4)
@@ -223,9 +171,31 @@ while robot.step(timestep) != -1:
             pass
         elif key==Keyboard.CONTROL+ord('N'):
         #BLOCKS ON MIDDLE SHELF TO BASKET
+            torso_lift_joint.setPosition(0)
             arm_4_joint.setPosition(2.1)
             arm_5_joint.setPosition(1.5)
             arm_6_joint.setPosition(-1.34)
+            pass
+            
+        # --- BLOCKS TOP SHELF --- #    
+        elif key==Keyboard.CONTROL+ord('T'):
+        #BLOCKS ON TOP SHELF
+            torso_lift_joint.setPosition(0.35)
+            arm_1_joint.setPosition(0.07)
+            arm_2_joint.setPosition(0.04)
+            arm_3_joint.setPosition(-1.8)
+            arm_4_joint.setPosition(1.4)
+            arm_5_joint.setPosition(-0.1)
+            arm_6_joint.setPosition(-0.3)
+            arm_7_joint.setPosition(0.1)
+            pass
+        elif key==Keyboard.CONTROL+ord('Y'):
+        #BLOCKS ON TOP SHELF TO BASKET
+            torso_lift_joint.setPosition(0.2)
+            arm_4_joint.setPosition(2.15)
+            arm_5_joint.setPosition(-1.9)
+            arm_6_joint.setPosition(1.39)
+            arm_7_joint.setPosition(-1.5)
             pass
             
         # --- GRIPPERS --- #
@@ -242,86 +212,19 @@ while robot.step(timestep) != -1:
             
         # ----- MOVE ROBOT MANUALLY ----- #
         elif key == keyboard.LEFT :
-            vL = -MAX_SPEED/4
-            vR = MAX_SPEED/4
+            vL = -MAX_SPEED/2.5
+            vR = MAX_SPEED/2.5
         elif key == keyboard.RIGHT:
-            vL = MAX_SPEED/4
-            vR = -MAX_SPEED/4
+            vL = MAX_SPEED/2.5
+            vR = -MAX_SPEED/2.5
         elif key == keyboard.UP:
-            vL = MAX_SPEED/4
-            vR = MAX_SPEED/4
+            vL = MAX_SPEED
+            vR = MAX_SPEED
         elif key == keyboard.DOWN:
-            vL = -MAX_SPEED/4
-            vR = -MAX_SPEED/4
+            vL = -MAX_SPEED
+            vR = -MAX_SPEED
         elif key == ord(' '):
             vL = 0
             vR = 0
-            #i = arm_1_joint.getPositionSensor()
-            #print(i)
-            
-        """
-        if key == ord('1'):
-        #main arm left
-            #arm_1_joint_position += 0.1
-            #arm_1_joint.setPosition(0.1)
-            if key == ord('1'):
-                for i in np.arange(0.07, 2.68, 0.01):
-                    arm_1_joint.setPosition(i)
-        elif key == ord('2'):
-        #main arm right
-            #arm_1_joint_position -= 0.1
-            #arm_1_joint.setPosition(-0.1)
-            if key == ord('2'):
-                for i in np.arange(2.68, 0.07, -0.01):
-                    arm_1_joint.setPosition(i)
-        elif key == ord('3'):
-        #main arm up
-            for i in np.arange(-1.5, 1.02, 0.01):
-                arm_2_joint.setPosition(i)
-        elif key == ord('4'):
-        #main arm down
-            for i in np.arange(1.02, -1.5, -0.01):
-                arm_2_joint.setPosition(i) 
-        elif key == ord('7'):
-        #main arm rotate left
-            #arm_3_joint.setPosition(0.1)
-            if key == ord('7'):
-                for i in np.arange(-3.46, 1.5, 0.01):
-                    arm_3_joint.setPosition(i)
-                    print(i)
-        elif key == ord('8'):
-        #main arm rotate right
-            if key == ord('8'):
-                for i in np.arange(1.5, -3.46, -0.01):
-                    arm_3_joint.setPosition(i)
-                    print(i)
-                #current_position = arm_3_joint.setPosition(i)
-                #print(i)
-                #print(current_position)
-        elif key == ord('9'):
-        #main arm rotate right
-            if key == ord('9'):
-                for i in np.arange(-0.32, 2.29, 0.01):
-                    arm_4_joint.setPosition(i)
-        elif key == ord('a'):
-        #main arm rotate right
-            if key == ord('a'):
-                for i in np.arange(2.29, -0.32, -0.01):
-                    arm_4_joint.setPosition(i)
-        elif key==Keyboard.CONTROL+ord('I'):
-            current_joint_positions = np.zeros(12)
-            joint_positions = my_chain1.forward_kinematics(current_joint_positions)
-            print("new", joint_positions)
-            
-        elif key == ord('5'):
-            # Close gripper, note that this takes multiple time steps...
-            robot_parts["gripper_left_finger_joint"].setPosition(0)
-            robot_parts["gripper_right_finger_joint"].setPosition(0)
-            pass
-        elif key == ord('6'):
-            #open gripper
-            robot_parts["gripper_left_finger_joint"].setPosition(0.045)
-            robot_parts["gripper_right_finger_joint"].setPosition(0.045)
-        """
    
 # ------------------------------------------------------------------       
